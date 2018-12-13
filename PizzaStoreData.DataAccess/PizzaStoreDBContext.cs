e60456edc1d3d897146a54d90ff2d314374cc5c1
John Pot <j.pot@digipen.edu>
2018-12-13 12:18:39 -0600

    Initial solution / projects creation

---
+++ PizzaStoreData.DataAccess/PizzaStoreDBContext.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PizzaStoreData.DataAccess
+{
+    public class PizzaStoreDBContext
+    {
+    }
+}
---
+++ PizzaStoreLibrary.library/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PizzaStoreLibrary.library
+{
+    public class Class1
+    {
+    }
+}
